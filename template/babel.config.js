2eb46e86d7226e2ae2ce3262a873b00ca705ef9b
fujia <fujia.site@outlook.com>
2022-06-18 17:24:31 +0800

    feat: added babel.config.js

---
+++ template/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    [
+      '@babel/plugin-transform-modules-commonjs', // transform ECMA modules to CommonJS
+      {
+        allowTopLevelThis: true,
+        loose: true,
+        // lazy: true,
+      },
+    ],
+  ],
+};
